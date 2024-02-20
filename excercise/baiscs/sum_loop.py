a72acbf3fe4f897d200b62636f6d45bf26d67b2f
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2024-02-20 13:01:18 +0500

    doc string

---
+++ excercise/baiscs/sum_loop.py
@@ -0,0 +1,3 @@
+for num in range(1, 11,):
+
+    print(num, num+num+1)
---
+++ lab/docstring.py
@@ -0,0 +1,8 @@
+def multi(n):
+    """function can take  value and retrun it's value """  # this is not comment they are used to document the code
+    print(n*2)
+
+
+multi(5)
+# used the double underscore to before and after doc
+multi(multi.__doc__)
