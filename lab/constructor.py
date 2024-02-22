c8f9becdeeb6c19fcb0f24a292574ef30b73478c
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2024-02-22 18:23:27 +0500

    constructor

---
+++ lab/constructor.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, n, a):
+        # print("i'm constructor")
+        self.name = n
+        self.age = a
+
+    def output(self):
+        print(f"my name is {self.name} and my age is {self.age}")
+
+
+# one method to access value
+p1 = Person("Adnan", 22)
+print(f"I'm {p1.name} and my age is {p1.age}")
+p2 = Person("Dawood", 23)
+p2.output()
