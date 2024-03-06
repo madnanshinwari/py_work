b66c831524bfa3e1d2d950ca6e2e12a1012c0c4c
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2024-03-06 21:55:49 +0500

    module

--- lab/modules/callmoducle.py
+++ lab/modules/callmoducle.py
@@ -1,2 +1,6 @@
-import mymodule
-mymodule.hello("Adnan")
+import mymodule  # import a file
+
+mymodule.hello("Adnan")  # call the mymodule file function
+
+a = mymodule.person["age"]
+print(a)
--- lab/modules/mymodule.py
+++ lab/modules/mymodule.py
@@ -1,2 +1,10 @@
 def hello(name):
     print("hello!", name)
+
+
+person = {
+    "name": "Adnan",
+    "age": 22,
+    "qualification": "BSSE",
+    "address": "peshawar"
+}
