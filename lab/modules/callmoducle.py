cfc716711cd8c443ca0703e5535161874c9c1aeb
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2024-03-06 22:04:24 +0500

    add from keyword

--- lab/modules/callmoducle.py
+++ lab/modules/callmoducle.py
@@ -1,6 +1,9 @@
-import mymodule  # import a file
+from mymodule import person  # import  only person from the module file
 
-mymodule.hello("Adnan")  # call the mymodule file function
+"""mymodule.hello("Adnan")  # call the mymodule file function
 
-a = mymodule.person["age"]
-print(a)
+a = mymodule.person["age"] # access the dictionary
+print(a)"""
+
+print(person["age"])  # when we use the from keyword  don't use module name
+print(hello("khan"))
